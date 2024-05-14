042c6294bdb16430da1483a0b3a100d68fde408c
Foad <fwad40901@gmail.com>
2024-05-15 02:55:12 +0330

    Create Rating Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
-
+// import './index.css';
+import StarRating from './components/StarRating.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
